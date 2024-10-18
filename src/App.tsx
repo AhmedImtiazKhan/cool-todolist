789e14794f2f1b4a34dc6f4f6bc994a2bf7e9891
Ahmed Imtiaz Khan <Ahmed.imtiaz2000@gmail.com>
2024-10-18 06:37:56 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -55,8 +55,6 @@ function App() {
           todoValue={todoValue}
           setTodoValue={setTodoValue}
         />
-        {}
-        {""}
         <TodoList
           handleEditTodos={handleEditTodos}
           handleDeleteTodos={handleDeleteTodos}
